d9258d79ad208e0129235c91ed61b4d46fd1a241
shahrukhpunjwani <shahrukhali2520@gmail.com>
2026-01-21 15:30:59 +0500

    changed the content

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -4,30 +4,55 @@ import Cookies from "js-cookie";
 import { PermissionModal } from "./StripeAccountPermissionModal";
 import { enqueueSnackbar } from "notistack";
 import CommunitiesList from "./CommunitiesList";
-import { useCreateStripeAccountMutation } from "../../services/userApi/userApi";
-import { useCheckStripeStatusQuery } from "../../services/dashboardApi/dashboardApi";
+import {
+  useCreateStripeAccountMutation,
+  useLazyCheckStripeStatusQuery,
+} from "../../services/userApi/userApi";
 import { useSelector } from "react-redux";
 
 const HomePage = () => {
   const user = useSelector((state) => state?.user?.user);
   const [showConfirmationModal, setShowConfirmationModal] = useState(false);
 
-  const { error: stripeError } = useCheckStripeStatusQuery(undefined, {
-    refetchOnMountOrArgChange: true,
-  });
+  const [checkStripeStatus, { isLoading }] = useLazyCheckStripeStatusQuery();
+
   const [createStripeAccount, { isLoading: createStripe }] =
     useCreateStripeAccountMutation();
 
-  useEffect(() => {
-    document.title = "Dashboard - giveXchange";
-    [("email", "signupEmail", "verifyEmail")].forEach((c) => Cookies.remove(c));
-  }, []);
+  const handleCheckStripeAccountStatus = async () => {
+    try {
+      const res = await checkStripeStatus().unwrap();
+      const status = res?.data?.accountStatus;
+
+      if (status === "active") {
+        setShowAddCommunityPopup(true);
+      } else if (status === "not_connected") {
+        setShowConfirmationModal(true);
+      } else if (status === "pending") {
+        setShowConfirmationModal(true);
+      } else {
+        enqueueSnackbar("Something went wrong. Try again.", {
+          variant: "error",
+        });
+      }
+    } catch (error) {
+      if (error?.status === 404) setShowConfirmationModal(true);
+      enqueueSnackbar(
+        error?.response?.data?.message ||
+          error?.data?.message ||
+          "Something went wrong.",
+        {
+          variant: "error",
+        },
+      );
+    }
+  };
 
   useEffect(() => {
-    if (stripeError?.status === 404) {
-      setShowConfirmationModal(true);
-    }
-  }, [stripeError]);
+    document.title = "Dashboard - giveXchange";
+    ["email", "signupEmail", "verifyEmail"].forEach((c) => Cookies.remove(c));
+    handleCheckStripeAccountStatus();
+  }, []);
 
   return (
     <main className="w-full p-5 rounded-[10px] bg-white custom-shadow min-h-[78.6vh]">
@@ -50,26 +75,28 @@ const HomePage = () => {
         <CommunitiesList limit={12} />
       </div>
 
-      <PermissionModal
-        handleCreateStripeAccount={async () => {
-          try {
-            const res = await createStripeAccount().unwrap();
-            if (res?.success) {
-              window.open(res.data.url, "_blank");
+      {!isLoading && (
+        <PermissionModal
+          handleCreateStripeAccount={async () => {
+            try {
+              const res = await createStripeAccount().unwrap();
+              if (res?.success) {
+                window.open(res.data.url, "_blank");
+              }
+            } catch (error) {
+              enqueueSnackbar(
+                error?.data?.message || "Something went wrong. Try again.",
+                { variant: "error" },
+              );
+            } finally {
+              setShowConfirmationModal(false);
             }
-          } catch (error) {
-            enqueueSnackbar(
-              error?.data?.message || "Something went wrong. Try again.",
-              { variant: "error" }
-            );
-          } finally {
-            setShowConfirmationModal(false);
-          }
-        }}
-        loading={createStripe}
-        showConfirmationModal={showConfirmationModal}
-        setShowConfirmationModal={setShowConfirmationModal}
-      />
+          }}
+          loading={createStripe}
+          showConfirmationModal={showConfirmationModal}
+          setShowConfirmationModal={setShowConfirmationModal}
+        />
+      )}
     </main>
   );
 };
